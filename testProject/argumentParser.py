7768cf7333d8d583240f82a9df1aeabdcafc7eca
kmhoran <kevin.michael.horan@gmail.com>
2018-02-26 13:08:59 -0800

    initial commit

---
+++ testProject/argumentParser.py
@@ -0,0 +1,21 @@
+import argparse
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description="test parser")
+    parser.add_argument("-t", "--twitter", action='store_true')
+    return parser.parse_args()
+
+def run(args=None):
+    if not args:
+        print ("No args given")
+    else: 
+        if args.twitter:
+            print("Let's tweet!")
+        else:
+            print("no tweeting today")
+
+
+if __name__ == "__main__":
+
+    args = arg_parse()
+    run(args)
