39c9e5f4cbc11516c3c85075e558a5bc03ec8b07
kmhoran <kevin.michael.horan@gmail.com>
2018-02-27 12:50:31 -0800

    more comments

--- testProject/argumentParser.py
+++ testProject/argumentParser.py
@@ -14,7 +14,7 @@ def run(args=None):
         else:
             print("no tweeting today")
 
-# comment some more
+# comment some
 if __name__ == "__main__":
     print("Entering argument parser!")
     args = arg_parse()
