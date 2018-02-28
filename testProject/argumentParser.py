4cbfba0e952acdbdc8dba04a92fc30de0e425dc9
kmhoran <kevin.michael.horan@gmail.com>
2018-02-27 18:33:51 -0800

    turns out i'm not using right python

--- testProject/argumentParser.py
+++ testProject/argumentParser.py
@@ -15,7 +15,7 @@ def run(args=None):
             print("no tweeting today")
 
 # 
-# comment dd
+# comment
 if __name__ == "__main__":
     print("Entering argument parser!")
     args = arg_parse()
