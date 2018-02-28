b7e7e68b766810d53c801ab6502ae93f9601c8d3
kmhoran <kevin.michael.horan@gmail.com>
2018-02-27 19:01:25 -0800

    I hate indents

--- testProject/argumentParser.py
+++ testProject/argumentParser.py
@@ -15,7 +15,7 @@ def run(args=None):
             print("no tweeting today")
 
 # d
-# comment
+# comments
 if __name__ == "__main__":
     print("Entering argument parser!")
     args = arg_parse()
